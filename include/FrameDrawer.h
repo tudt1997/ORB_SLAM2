31b19367d1638305d43ae9456f32dbac2668157f
Alkaid <zgf574564920@gmail.com>
2019-01-22 01:26:33 -0500

    port to opencv4

--- include/FrameDrawer.h
+++ include/FrameDrawer.h
@@ -26,6 +26,7 @@
 #include "Map.h"
 
 #include<opencv2/core/core.hpp>
+#include<opencv2/imgproc/imgproc_c.h>
 #include<opencv2/features2d/features2d.hpp>
 
 #include<mutex>
--- include/PnPsolver.h
+++ include/PnPsolver.h
@@ -51,7 +51,7 @@
 #ifndef PNPSOLVER_H
 #define PNPSOLVER_H
 
-#include <opencv2/core/core.hpp>
+#include <opencv2/core/core_c.h>
 #include "MapPoint.h"
 #include "Frame.h"
 
